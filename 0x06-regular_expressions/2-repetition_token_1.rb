8bfa8bd99b503a75049b84e544549d833bf2327e
Meyvel22 <meyvel22@gmail.com>
2022-08-23 09:10:07 -0700

    Task 0-3

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# A Ruby script that matches the regular expression hn with 0 or 1 occurrences
+# of b and 0 or 1 occurrences of t in between h and n
+
+puts ARGV[0].scan(/hb?tn/).join
